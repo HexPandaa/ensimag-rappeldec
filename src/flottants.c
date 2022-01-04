5c031edc0d1fcba6049ff441d8b0cdafa1e4f8d3
HexPandaa <47880094+HexPandaa@users.noreply.github.com>
2022-01-04 14:55:36 +0100

    Implement flottants

--- src/flottants.c
+++ src/flottants.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(void)
 {
+    printf("%e\n", 0.1f + 0.2f - 0.3f);
+    printf("%e\n", 0.1 + 0.2 - 0.3);
+    printf("%Le\n", 0.1L + 0.2L - 0.3L);
     return EXIT_SUCCESS;
 }
