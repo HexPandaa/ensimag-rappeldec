e0863db80ca22b509e6975b9b4ad453ea56d22db
HexPandaa <47880094+HexPandaa@users.noreply.github.com>
2022-01-04 15:26:46 +0100

    Implement fqsort

--- src/fqsort.c
+++ src/fqsort.c
@@ -6,26 +6,31 @@
    Ajouter la fonction de comparaison du tri ici
 */
 
-const int TAILLE = 100;
+const unsigned int TAILLE = 100;
 
-int main(void)
-{
+int dcCompare(const void *a, const void *b) {
+    double _a = carg(*((const double complex *)a));
+    double _b = carg(*((const double complex *)b));
+    if (_a == _b) return 0;
+    else if (_a < _b) return -1;
+    return 1;
+}
+
+int main(void) {
     /* initialisation du tableau */
     double complex tableau[TAILLE];
     srand48(123456);
-    for(int i = 0; i < TAILLE; i++) {
+    for (unsigned int i = 0; i < TAILLE; i++) {
         tableau[i] = drand48() + (drand48() * 1.0i);
     }
 
-    /**
-       Mettre le code d'appel de qsort ici
-    */
+    qsort(tableau, (unsigned long) TAILLE, sizeof(double complex), dcCompare);
 
     /**
        Vérification du tri
     */
-    for(int i = 0; i < TAILLE - 1; i++) {
-        assert(carg(tableau[i]) <= carg(tableau[i+1]));
+    for (unsigned int i = 0; i < TAILLE - 1; i++) {
+        assert(carg(tableau[i]) <= carg(tableau[i + 1]));
     }
 
     return EXIT_SUCCESS;
